7c96b1fac4e527eca48aa2e5e4a79f662a14e99a
Jon Bågling <bagling@gmail.com>
2025-10-02 09:28:42 +0200

    move create new hero to function, add menu items

--- v02/bin/v02.dart
+++ v02/bin/v02.dart
@@ -1,20 +1,5 @@
-// import 'package:v02/v02.dart' as v02;
-import 'package:v02/hero.dart';
 import 'package:v02/v02.dart';
 
 void main(List<String> arguments) {
-  var name = getUserInput<String>('Enter hero name: ');
-  var strength = getUserInput<int>('Enter hero strength (number): ');
-  var gender = getUserInput<String>('Enter hero gender: ');
-  var race = getUserInput<String>('Enter hero race: ');
-  var alignment = getUserInput<String>('Enter hero alignment (good, evil): ');
-  // var thor = Hero.fromJson({
-  //   "name": "Thor",
-  //   "powerstats": {"strength": 100},
-  //   "appearance": {"gender": "Male", "race": "Asgardian"},
-  //   "biography": {"alignment": "good"},
-  // });
-
-  var hero = Hero.add(name, strength, gender, race, alignment);
-  print('Saved hero: ${hero.toString()}!');
+  showMainMenu();
 }
--- v02/lib/v02.dart
+++ v02/lib/v02.dart
@@ -1,5 +1,7 @@
 import 'dart:io';
 
+import 'package:v02/hero.dart';
+
 T getUserInput<T>(String prompt) {
   stdout.write(prompt);
   var input = stdin.readLineSync();
@@ -8,4 +10,50 @@ T getUserInput<T>(String prompt) {
   } else {
     return input as T;
   }
+}
+
+void showMainMenu() {
+  print('Welcome, make your choice below:');
+  print('1. Add Hero');
+  print('2. View Heroes');
+  print('3. Search Heroes');
+  print('4. Exit');
+  var menuItem = stdin.readLineSync();
+
+  print('\nYou selected: $menuItem\n');
+  switch (menuItem) {
+    case '1':
+      addNewHero();
+      break;
+    case '2':
+      // View Heroes
+      break;
+    case '3':
+      // Search Heroes
+      break;
+    case '4':
+      print('Goodbye for now!');
+      exit(0);
+    default:
+      print('Invalid option. Please try again.\n');
+      showMainMenu();
+  }
+}
+
+void addNewHero() {
+  var name = getUserInput<String>('Enter hero name: ');
+  var strength = getUserInput<int>('Enter hero strength (number): ');
+  var gender = getUserInput<String>('Enter hero gender: ');
+  var race = getUserInput<String>('Enter hero race: ');
+  var alignment = getUserInput<String>('Enter hero alignment (good, evil): ');
+  // var thor = Hero.fromJson({
+  //   "name": "Thor",
+  //   "powerstats": {"strength": 100},
+  //   "appearance": {"gender": "Male", "race": "Asgardian"},
+  //   "biography": {"alignment": "good"},
+  // });
+
+  Hero.add(name, strength, gender, race, alignment);
+  print('Hero $name successfully added!\n');
+  showMainMenu();
 }
